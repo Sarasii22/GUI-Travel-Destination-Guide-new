dd6ec027288a90e7e57cdcd57f128367e2958e7c
testuser <testuser@gmail.com>
2025-01-15 02:17:38 +0530

    hero section

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import Navbar from './Components/Navbar/Navbar'
 //import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Hero from './Components/Hero/Hero'
+import About from './Pages/About/About';
 
 function App() {
   
@@ -12,6 +13,7 @@ function App() {
        
       <Navbar/>
       <Hero/>
+      <About/>
        
     </>
   );
--- frontend/src/Pages/About/About.jsx
+++ frontend/src/Pages/About/About.jsx
@@ -1,9 +1,36 @@
 import React from 'react'
 import './About.css'
+import aboutimg from '../../assets/about.webp'
 const About = () => {
   return (
-    <div>
-      
+    <div className = "about" > 
+      <div className="about-image">
+        <img src ={aboutimg} alt = " "/>
+      </div>
+      <div className="about-text">
+        <div>
+          <h4>Explore with us</h4>
+          <h2>Choose dream destination for explore <span className="highlight">sri lanka</span></h2>
+          <p>Discover sri lanka of wonders! select from our curated list of dream destinations and start your journey to explore breathtaking landscapses, vibrant cultures, and unforgattable experiences.</p>
+          
+          
+        </div>
+        <div className="stats">
+          <div className="stat">
+            <h3>10</h3>
+            <p>years experiences</p>
+          </div>
+          <div className="stat">
+            <h3>54+</h3>
+            <p>destinations celebrations </p>
+          </div>
+          <div className="stat">
+            <h3>230+</h3>
+            <p>happy customers</p>
+          </div>
+        </div>
+      </div>
+
     </div>
   )
 }
