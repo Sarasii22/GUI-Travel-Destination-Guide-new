a8ee4c9feef8b26c8d8311fc282208474a5c35b5
testuser <testuser@gmail.com>
2025-01-19 07:43:03 +0530

    searchbar

---
+++ frontend/src/shared/SearchBar/SearchBar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './SearchBar.css'
+
+const SearchBar = () => {
+  return (
+    <div className='search-bar'>
+      <form className='search-bar-form'>
+        
+      </form>  
+    </div>
+  )
+}
+
+export default SearchBar
