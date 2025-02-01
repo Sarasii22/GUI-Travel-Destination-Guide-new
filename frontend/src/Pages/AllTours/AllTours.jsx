ff669460a016c2f3d8cba74aabf29c00719251f8
testuser <testuser@gmail.com>
2025-02-01 12:43:47 +0530

    as

--- frontend/src/Pages/AllTours/AllTours.jsx
+++ frontend/src/Pages/AllTours/AllTours.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import CommonSection from '../../Components/CommonSection/CommonSection'
+//import Subscribe from '../../Components/Subscribe/Subscribe'
 const AllTours = () => {
   return (
     <>
       <h1>this is the tours page</h1>
       <CommonSection title='All Tours'/>
+      
     </>
   )
 }
