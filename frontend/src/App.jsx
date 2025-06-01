30b8de414c7756b6d8e5cd78d20c0426882245a6
Sarasi <sarasimadahasi@gmail.com>
2025-06-01 22:37:37 +0530

    vercel

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './Pages/Home/Home';
 import Register from './Pages/Register/Register';
